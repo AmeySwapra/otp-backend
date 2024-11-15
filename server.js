0675613b6e12663ef3ed181eb7086fe6c40a0160
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-11-15 16:58:06 +0530

    origin added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'], 
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://swapra-otp-verification.netlify.app'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true,
     optionsSuccessStatus: 200,
